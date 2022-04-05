2433419f9592a37922c976edc1a975d4d9ff5ba4
Nick <kolan.eremeev1001@gmail.com>
2022-04-05 22:25:15 +0300

    Names data

---
+++ Assets/Scripts/NamesData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Names Data", menuName = "Create Names Data")]
+public class NamesData : ScriptableObject
+{
+    [SerializeField] private string[] _names = { "Blabbermouth", "Hercules", "Konversation", "Lollipop", "Hey", "Bigmouth", "Galore", "Jovial", "Supersmart", "Bleep Bloop", "The Monkey", "Volter", "Fintech", "SuchBot", "Neobot", "Funny Guy", "Dimwit", "Okay Sir", "Cybernetic", "Digital Dave", "Mr. Robot", "Pepper Bot", "Baxi", "Tutorial", "CloudBot", "Cometbot", "Comet Bot", "Albus", "Anthony", "Brooke" ,"Bumper", "Chandler", "Charlotte", "Smart One", "Abracadabra" };
+
+    public string[] Names => _names;
+}
