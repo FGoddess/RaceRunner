2574b263b1a9d4b86e43ba220e2a9b92d1825303
Nick <kolan.eremeev1001@gmail.com>
2022-04-12 01:08:01 +0300

    script for debug

---
+++ Assets/Scripts/Debugger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Debugger : MonoBehaviour
+{
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadScene();
+        }
+    }
+}
