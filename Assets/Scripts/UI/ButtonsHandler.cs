461d1c9af3f6dc14fb7bce3863d365542fd820a7
Nick <kolan.eremeev1001@gmail.com>
2022-04-12 01:07:23 +0300

    level system

--- Assets/Scripts/UI/ButtonsHandler.cs
+++ Assets/Scripts/UI/ButtonsHandler.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ButtonsHandler : MonoBehaviour
 {
-    public void LoadGameScene()
+    public void LoadLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int id = 1;
+
+        if (PlayerPrefs.HasKey("Level"))
+        {
+            id = PlayerPrefs.GetInt("Level");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Level", id);
+        }
+
+        SceneManager.LoadScene($"Level {id}");
     }
     
     public void LoadMainMenuScene()
--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 public class GameOverScreen : MonoBehaviour
 {
-    [SerializeField] private PlayerMover _player;
     [SerializeField] private CanvasGroup _winPanel;
     [SerializeField] private CanvasGroup _lossPanel;
+
+    [SerializeField] private int _maxLevel = 3;
 
     public void EndGame(bool isWin)
     {
         ActivatePanel(isWin ? _winPanel : _lossPanel);
+
+        if(isWin)
+        {
+            var level = PlayerPrefs.GetInt("Level", 1);
+            ++level;
+
+            if(level >= _maxLevel)
+            {
+                PlayerPrefs.SetInt("Level", 1);
+                return;
+            }
+
+            PlayerPrefs.SetInt("Level", ++level);
+        }
     }
 
     private void ActivatePanel(CanvasGroup panel)
