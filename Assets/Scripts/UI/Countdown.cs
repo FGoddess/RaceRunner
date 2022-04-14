d446278ff765a2ab0c3915ad559c4506b21116f2
Nick <kolan.eremeev1001@gmail.com>
2022-04-15 01:23:59 +0300

    countdown before start

---
+++ Assets/Scripts/UI/Countdown.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class Countdown : MonoBehaviour
+{
+    private TextMeshProUGUI _countdown;
+    private int _delay = 3;
+
+    public event Action CountdownEnded;
+
+    private void Awake()
+    {
+        _countdown = GetComponent<TextMeshProUGUI>();
+    }
+
+    private IEnumerator Start()
+    {
+        while (_delay > 0)
+        {
+            _countdown.text = _delay.ToString();
+            yield return new WaitForSeconds(1f);
+            --_delay;
+
+            if(_delay == 0)
+            {
+                _countdown.text = "GO!";
+                CountdownEnded?.Invoke();
+                yield return new WaitForSeconds(1f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
