b6c1a313bf15616c9360c7e1bf54441442a102e0
Nick <kolan.eremeev1001@gmail.com>
2022-04-16 00:51:24 +0300

    meta changes

--- Assets/Scripts/ParticipantKiller.cs
+++ Assets/Scripts/ParticipantKiller.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ParticipantKiller : MonoBehaviour, IInitializable
 {
+    [SerializeField] private PlayerLevel _playerLevel;
     [SerializeField] private GameOverScreen _gameOverScreen;
-    private PlayerData _player;
 
+    private PlayerData _player;
     private List<BotData> _bots;
 
     public void KillLastParticipant()
@@ -34,6 +35,7 @@ public class ParticipantKiller : MonoBehaviour, IInitializable
 
         if(_bots.Count == 0)
         {
+            _playerLevel.IncreaseLevel();
             _player.GetComponent<PlayerMover>().GameOver();
             _gameOverScreen.EndGame(true);
         }
---
+++ Assets/Scripts/UI/LevelText.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class LevelText : MonoBehaviour
+{
+    [SerializeField] private PlayerLevel _playerLevel;
+    [SerializeField] private bool _isMainMenu;
+
+    private TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _text.text = _isMainMenu ?  $"Ваш уровень: {_playerLevel.Level}" : $"Уровень повышен!\nТекущий: {_playerLevel.LevelNumber + 1}";
+    }
+}
