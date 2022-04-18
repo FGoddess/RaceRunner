991a03d74e1975d9cfab5c183f2db5b2d500a731
Nick <kolan.eremeev1001@gmail.com>
2022-04-19 02:08:06 +0300

    shop improvement

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
@@ -7,11 +8,24 @@ public class Shop : MonoBehaviour
 
     [SerializeField] private Material _playerMaterial;
 
+    [SerializeField] private YandexSDK _yandexSDK;
+
     private ShopItem _equpiedShopItem;
     private Skin _equpiedSkin;
 
     private void Awake()
     {
+        _yandexSDK.onRewardedAdReward += PurchaseSkin;
+        UpdateShop();
+    }
+
+    private void UpdateShop()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
         foreach (var skin in _skins)
         {
             var temp = Instantiate(_shopItemPrefab, transform);
@@ -19,36 +33,52 @@ public class Shop : MonoBehaviour
             var item = temp.GetComponent<ShopItem>();
 
             item.Image.sprite = skin.Sprite;
-            item.ButtonText.text = skin.IsPurchased ? skin.IsEqupied ? "Выбрано" : "Выбрать" : "Купить";
+            item.ButtonText.text = skin.IsPurchased ? skin.IsEqupied ? "Выбрано" : "Выбрать" : "Смотреть видео";
 
             if (skin.IsEqupied)
             {
                 _equpiedSkin = skin;
                 _equpiedShopItem = item;
-                item.ButtonText.color = Color.yellow;
-            }
-            else if(skin.IsPurchased)
-            {
                 item.ButtonText.color = Color.green;
             }
-
-            item.Button.onClick.AddListener(() => Purchase(item, skin));
+            else if (skin.IsPurchased)
+            {
+                item.ButtonText.color = Color.cyan;
+                item.Button.onClick.AddListener(() => EquipSkin(item, skin));
+            }
+            else
+            {
+                item.Button.onClick.AddListener(() => /*PurchaseSkin(skin.MaterialColor.ToString())*/_yandexSDK.ShowRewarded(skin.MaterialColor.ToString()));
+            }
         }
     }
 
-    private void Purchase(ShopItem shopItem, Skin skin)
+    private void PurchaseSkin(string item)
+    {
+        Debug.Log(item);
+
+        var skin = _skins.FirstOrDefault(s => s.MaterialColor.ToString() == item);
+        skin.IsPurchased = true;
+        skin.IsEqupied = true;
+        _equpiedSkin.IsEqupied = false;
+        _equpiedSkin = skin;
+
+        _playerMaterial.color = skin.MaterialColor;
+
+        UpdateShop();
+    }
+
+    private void EquipSkin(ShopItem shopItem, Skin skin)
     {
         shopItem.ButtonText.text = "Выбрано";
-        shopItem.ButtonText.color = Color.yellow;
+        shopItem.ButtonText.color = Color.green;
         _equpiedShopItem.ButtonText.text = "Выбрать";
 
         skin.IsEqupied = true;
-        skin.IsPurchased = true;
         _playerMaterial.color = skin.MaterialColor;
 
         _equpiedSkin.IsEqupied = false;
-
-        _equpiedShopItem.ButtonText.color = Color.green;
+        _equpiedShopItem.ButtonText.color = Color.cyan;
 
         _equpiedSkin = skin;
         _equpiedShopItem = shopItem;
