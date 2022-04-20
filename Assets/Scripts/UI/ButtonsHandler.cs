f10e4bf8a9ec8ea0164ee9385068aa4caa9b5b62
Nick <kolan.eremeev1001@gmail.com>
2022-04-20 16:39:00 +0300

    ads for level skip

--- Assets/Scripts/UI/ButtonsHandler.cs
+++ Assets/Scripts/UI/ButtonsHandler.cs
@@ -1,10 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonsHandler : MonoBehaviour
 {
+    private void Start()
+    {
+        YandexSDK.Instance.onRewardedAdReward += LoadLevelAfterRewardedAd;
+    }
+
+    private void OnDestroy()
+    {
+        YandexSDK.Instance.onRewardedAdReward -= LoadLevelAfterRewardedAd;
+    }
+
     public void LoadLevel()
     {
         int id = 1;
@@ -20,7 +28,12 @@ public class ButtonsHandler : MonoBehaviour
 
         SceneManager.LoadScene($"Level {id}");
     }
-    
+
+    private void LoadLevelAfterRewardedAd(string value)
+    {
+        LoadLevel();
+    }
+
     public void LoadMainMenuScene()
     {
         SceneManager.LoadScene(0);
@@ -29,5 +42,15 @@ public class ButtonsHandler : MonoBehaviour
     public void LoadTestLevel()
     {
         SceneManager.LoadScene("TestLevel");
+    }
+
+    public void MuteAudio()
+    {
+        AudioPlayer.Instance.Mute();
+    }
+
+    public void ShowRewardedAd()
+    {
+        YandexSDK.Instance.ShowRewarded(string.Empty);
     }
 }
